f6f41249fa9080762672a3d3b66370be5a016ede
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-11-18 17:45:44 -0800

    "    changed urls and updated functions  "

--- drf_jwt_capstone_backend/games_owned/urls.py
+++ drf_jwt_capstone_backend/games_owned/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import GamesOwnedAdd, GamesOwnedList, GamesOwnedDetail
+from .views import GamesOwnedAdd, GamesOwnedList, GamesOwnedDetail, GamesOwnedAPI
 
 urlpatterns = [
-    path('', GamesOwnedAdd.as_view()),
-    path('<int:pk>/', GamesOwnedDetail.as_view()),
+    # path('', GamesOwnedAdd.as_view()),
+    # path('<int:pk>/', GamesOwnedDetail.as_view()),
     path('users/<int:pk_user>/', GamesOwnedList.as_view()),
+
+    path('users/<int:user_id>/games/<int:game_id>/', GamesOwnedAPI.as_view()),
 ]
--- drf_jwt_capstone_backend/games_owned/views.py
+++ drf_jwt_capstone_backend/games_owned/views.py
@@ -10,38 +10,91 @@ from .models import GamesOwned
 from .serializers import GamesOwnedSerializer
 
 
+# TypeError: Object of type QuerySet is not JSON serializable
+# return JsonResponse(games_owned, safe=False)
+# we are building our own response dictionary manually
+# because when we try to return games_owned as json response (see ^)
+# the joined game/user weren't showing up in json)
+# we are building a "simpler" object/dict that will be serialized as expected
+# TODO use Serializer pattern since it handles mapping django db objects to json.
+def build_owned_game_for_json_serialization(owned_game):
+    return {
+        "id": owned_game.id,
+        "game": {
+            "id": owned_game.game.id,
+            "title": owned_game.game.title,
+            "publisher": owned_game.game.publisher,
+            "genre": owned_game.game.genre,
+            "image": owned_game.game.image,
+            "system": owned_game.game.system,
+            "release_date": owned_game.game.release_date,
+            "value": owned_game.game.value,
+            "rating": owned_game.game.rating,
+        },
+        "owner_rating": owned_game.owner_rating,
+        "review": owned_game.review
+    }
+
+class GamesOwnedAPI(APIView):
+    permission_classes = [AllowAny]
+
+    def get_object(self, user_id, game_id):
+        game_owned = GamesOwned.objects\
+                .filter(user_id=user_id, game_id=game_id) \
+                .select_related()
+        if len(game_owned) == 0:
+            raise Http404
+
+        return game_owned[0]
+
+    # get single games owned record
+    def get(self, request, user_id, game_id):
+        game_owned = self.get_object(user_id, game_id)
+
+        game = build_owned_game_for_json_serialization(game_owned)
+
+        return JsonResponse(game, safe=False)
+
+    def post(self, request, user_id, game_id):
+        print("add owned game 2")
+        games_owned = {
+            "user_id": user_id,
+            "game_id": game_id,
+            "owner_rating": 0,
+            "review": "N/A"
+        }
+        serializer = GamesOwnedSerializer(data=games_owned)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, user_id, game_id):
+        print("is delete getting called")
+        game_owned = self.get_object(user_id, game_id)
+        serializer = GamesOwnedSerializer(game_owned)
+        print("I am delete function")
+        game_owned.delete()
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+
+    def patch(self, request, user_id, game_id):  # pk = GamesOwnedId
+        game_owned = self.get_object(user_id, game_id)
+        game_owned.owner_rating = request.data['owner_rating']
+        game_owned.review = request.data['review']
+
+        # todo confirm game owned is not null (i.e. exists)
+        serializer = GamesOwnedSerializer(game_owned)
+        serializer.update(game_owned, )
+        return Response(serializer.data)
+
+
 class GamesOwnedList(APIView):
     def get(self, request, pk_user):
         games_owned = GamesOwned.objects \
             .filter(user_id=pk_user) \
             .select_related()
 
-        # TypeError: Object of type QuerySet is not JSON serializable
-        # return JsonResponse(games_owned, safe=False)
-        # we are building our own response dictionary manually
-        # because when we try to return games_owned as json response (see ^)
-        # the joined game/user weren't showing up in json)
-        # we are building a "simpler" object/dict that will be serialized as expected
-        # TODO use Serializer pattern since it handles mapping django db objects to json.
-        def build_owned_game(owned_game):
-            return {
-                "id": owned_game.id,
-                "game": {
-                    "id": owned_game.game.id,
-                    "title": owned_game.game.title,
-                    "publisher": owned_game.game.publisher,
-                    "genre": owned_game.game.genre,
-                    "image": owned_game.game.image,
-                    "system": owned_game.game.system,
-                    "release_date": owned_game.game.release_date,
-                    "value": owned_game.game.value,
-                    "rating": owned_game.game.rating,
-                },
-                "owner_rating": owned_game.owner_rating,
-                "review": owned_game.review
-            }
-
-        games = list(map(build_owned_game, list(games_owned)))
+        games = list(map(build_owned_game_for_json_serialization, list(games_owned)))
         for game in games:
             print(game)
 
@@ -49,6 +102,7 @@ class GamesOwnedList(APIView):
 
 
 
+# deprecated
 class GamesOwnedAdd(APIView):
     permission_classes = [AllowAny]
 
@@ -90,7 +144,7 @@ class GamesOwnedDetail(APIView):
         game_owned.delete()
         return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
 
-    def patch(self, request, pk): # pk = GamesOwnedId
+    def patch(self, request, pk):  # pk = GamesOwnedId
         # request = {
         #     "id": pk,
         #     "owner_rating": request.data['owner_rating'],
