b851055b2b092ce36f6d530d05deb438aa949c08
steven-halla <69405488+steven-halla@users.noreply.github.com>
2021-12-16 00:46:56 -0800

    "  improving models  "

--- drf_jwt_capstone_backend/games_owned/models.py
+++ drf_jwt_capstone_backend/games_owned/models.py
@@ -7,5 +7,7 @@ class GamesOwned(models.Model):
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     game = models.ForeignKey(Game, on_delete=models.DO_NOTHING)
     notes = models.CharField(max_length=150)
+    fixed_value = models.IntegerField()
+
 
 
